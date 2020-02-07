000695c1a70f387503bd0a83fcfd3a7c7b19c0d8
jmparavicini <j.m.paravicini@gmail.com>
2020-02-07 08:27:16 +0100

    Add project files.

---
+++ TCPServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+using JetBrains.Annotations;
+
+namespace ChatServer
+{
+    [UsedImplicitly]
+    internal class Program: IDisposable
+    {
+        private const int Port = 6700;
+        private readonly TcpClient _client = new TcpClient("localhost", Port);
+
+        private static void Main(string[] args)
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool dispose)
+        {
+            if (dispose)
+            {
+                _client.Dispose();
+            }
+        }
+    }
+}
